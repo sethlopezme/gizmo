c417cb0f2a57e40fbd09c9a0ac58a7852169f5bd
Seth Lopez <seth@sethlopez.me>
2016-02-26 20:41:00 -0500

    Move the main file into the bin directory

--- src/bin/gizmo.rs
+++ src/bin/gizmo.rs
@@ -1,4 +1,5 @@
 extern crate gizmo;
+use gizmo::scaffolder;
 
 #[macro_use]
 extern crate clap;
@@ -10,30 +11,51 @@ fn main() {
                    .version(crate_version!())
                    .settings(&[AppSettings::SubcommandRequiredElseHelp,
                                AppSettings::VersionlessSubcommands])
-                   .subcommand(SubCommand::with_name("new")
+                   .usage("gizmo [SUBCOMMAND] [FLAGS]")
+                   .subcommand(SubCommand::with_name("init")
                                    .about("Scaffold a new site at the given path")
-                                   .arg(Arg::with_name("name")
-                                            .takes_value(true)
-                                            .value_name("PATH")
+                                   .arg(Arg::with_name("path")
                                             .required(true)
-                                            .help("The location of the scaffolded site")))
-                   .subcommand(SubCommand::with_name("build").about("Build the site"))
+                                            .value_name("PATH")
+                                            .default_value(".")
+                                            .help("The path for the new site"))
+                                   .arg(Arg::with_name("force")
+                                            .long("force")
+                                            .help("Force file creation and overwrite existing \
+                                                   files at the given path")))
+                   .subcommand(SubCommand::with_name("generate")
+                                   .about("Generate the site from source files")
+                                   .arg(Arg::with_name("path")
+                                            .required(true)
+                                            .value_name("PATH")
+                                            .default_value(".")
+                                            .help("The path containing a gizmo config file")))
                    .subcommand(SubCommand::with_name("serve")
                                    .about("Serve the site locally")
                                    .arg(Arg::with_name("port")
-                                            .long("port")
                                             .takes_value(true)
                                             .value_name("PORT")
+                                            .long("port")
                                             .help("Set the port for the server"))
-                                   .arg(Arg::with_name("no-watch")
-                                            .long("no-watch")
-                                            .help("Don't watch source files for changes")))
+                                   .arg(Arg::with_name("no-regenerate")
+                                            .long("no-regenerate")
+                                            .help("Don't regenerate when source files change")))
                    .get_matches();
 
     match args.subcommand() {
-        ("new", Some(subcmd)) => println!("Subcommand: {}", "new"),
-        ("build", Some(subcmd)) => println!("Subcommand: {}", "build"),
+        ("init", Some(subcmd)) => {
+            let path = subcmd.value_of("path").unwrap();
+            let force = subcmd.is_present("force");
+
+            println!("Beep boop. Scaffolding your new site.");
+
+            match scaffolder::scaffold(path, force) {
+                Ok(_) => println!("Beep boop. Scaffolding complete!"),
+                Err(e) => panic!("ERROR: {}", e),
+            }
+        }
+        ("generate", Some(subcmd)) => println!("Subcommand: {}", "generate"),
         ("serve", Some(subcmd)) => println!("Subcommand: {}", "serve"),
-        _ => panic!(),
-    }
+        _ => panic!("Unknown subcommand."),
+    };
 }
